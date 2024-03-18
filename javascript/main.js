5acd4bae9e0883e49a6a095e21b9f69a04a839b8
rjarrin <jarrin.ricardo@gmail.com>
2024-03-17 20:50:04 -0400

    Layout updated

--- javascript/main.js
+++ javascript/main.js
@@ -71,6 +71,8 @@ function createAnnouncementCard() {
         { type: "title", text: "Title B"},
         { type: "text", text: "Here is some even more text"},
         { type: "title", text: "Title C"},
+        { type: "text", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."},
+        { type: "title", text: "Title D"},
         { type: "text", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}
     ];
     // Create and append the content elements
